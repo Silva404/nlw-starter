876af2bae1add9b169ddd59679c9396a3b1556b2
Silva404 <brenosilva0252@gmail.com>
2020-06-03 18:58:56 -0300

    quase pronto o front end

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -19,7 +19,7 @@ function changeCities(event) {
   const stateInput = document.querySelector('input[name=state]');
 
   const indexOfSelectedState = event.target.selectedIndex;
-  stateInput.value = event.target.options[indexOfSelectedState].text  
+  stateInput.value = event.target.options[indexOfSelectedState].text
 
   const ufValue = event.target.value;
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
@@ -32,7 +32,7 @@ function changeCities(event) {
     .then(cities => {
       for (const city of cities) {
         citySelect.innerHTML += `<option value='${city.nome}'>${city.nome}</option>`;
-        
+
         citySelect.disabled = false;
       }
     })
@@ -50,14 +50,25 @@ for (const item of itemsToCollect) {
   item.addEventListener('click', handleSelectedItem);
 }
 
+let selectedItems = [1, 2, 3, 4, 5, 6];
+
 function handleSelectedItem(event) {
   // event.target é o evento que ocorreu e o seu alvo, que no caso é o evento de click em um Li especifico
   const itemLi = event.target;
-
   //removendo ou adicionando um classe a um item
   itemLi.classList.toggle('selected');
-
-
   //pegando o id de cada item pelo seu dataX
   const itemId = itemLi.dataset.id;
+
+
+  // const alreadySelected = selectedItems.findIndex(item => {
+  //   const itemFound = item == itemId;
+  //   return itemFound;
+  // })
+
+  const alreadySelected = selectedItems.findIndex( item => item == itemId );
+
+  //verificar quais tems selecionados e pegar cada um
+
+  //se já estiver selecionado, tirar da seleção
 }
