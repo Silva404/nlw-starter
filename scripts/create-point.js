3171393f2f103cc5f1032d133c56cd77e76ee6a8
Silva404 <brenosilva0252@gmail.com>
2020-06-03 13:24:34 -0300

    deu bom

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,51 +1,91 @@
+// function populateUFs() {
+//   const ufSelect = document.querySelector('select[name=uf]');
+
+//   //chamar a lista do link e receber seus dados
+//   fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+//     //converter esse json
+//     .then(res => res.json())
+//     //minha função para pegar cada estado e colocar como nova option
+//     .then(states => {
+
+//       for (const state of states) {
+//         ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+//       }
+//       //pega os valores do objeto estado e sua propriedade nome
+//       //fiz um loop pra pegar cada objeto individualmente pelo seu nome
+
+//     })
+// }
+
+// populateUFs()
+
+
+// function getCities(event) {
+//   const citySelect = document.querySelector('select[name=city]');
+//   const stateInput = document.querySelector('input[name=state]');
+
+//   const ufValue = event.target.value;
+
+//   const indexOfSelectedState = event.target.selectedIndex;
+//   stateInput.value = event.target.options[indexOfSelectedState].text;
+
+//   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/distritos`;
+
+//   fetch(url)
+//     .then(res => res.json())
+//     .then(cities => {
+
+//       for (const city of cities) {
+//         citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+//       }
+
+//       citySelect.disabled = false;
+//     })
+
+// }
+
+// //serve só pra sempre que eu mudar o estado, o consolem me notificar
+// //useless
+// document
+//   .querySelector('select[name=uf')
+//   .addEventListener('change', getCities)
+
+
+
 function populateUFs() {
   const ufSelect = document.querySelector('select[name=uf]');
 
-  //chamar a lista do link e receber seus dados
   fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-    //converter esse json
     .then(res => res.json())
-    //minha função para pegar cada estado e colocar como nova option
     .then(states => {
-
       for (const state of states) {
-        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        ufSelect.innerHTML += `<option value='${state.id}'>${state.nome}</option>`
       }
-      //pega os valores do objeto estado e sua propriedade nome
-      //fiz um loop pra pegar cada objeto individualmente pelo seu nome
-
+      
     })
 }
 
 populateUFs()
 
 
-function getCities(event) {
+function changeCities() {
   const citySelect = document.querySelector('select[name=city]');
-  const stateInput = document.querySelector('input[name=state]');
 
   const ufValue = event.target.value;
-
-  const indexOfSelectedState = event.target.selectedIndex;
-  stateInput.value = event.target.options[indexOfSelectedState].text;
-
-  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/distritos`;
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
   fetch(url)
-    .then(res => res.json())
-    .then(cities => {
+  .then(res => res.json())
+  .then(cities => {
+    for(const city of cities) {
+    citySelect.innerHTML += `<option value='${city.id}'>${city.nome}</option>`;
 
-      for (const city of cities) {
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
-      }
-
-      citySelect.disabled = false;
-    })
-
+    citySelect.disabled = false;
+    }
+  })
 }
 
-//serve só pra sempre que eu mudar o estado, o consolem me notificar
-//useless
+
 document
-  .querySelector('select[name=uf')
-  .addEventListener('change', getCities)
+  .querySelector('select[name=uf]')
+  .addEventListener('change', changeCities);
