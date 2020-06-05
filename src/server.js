401ba68531cb300624a371b39868047e2fefe927
Silva404 <brenosilva0252@gmail.com>
2020-06-04 23:26:01 -0300

    terminando a aula 4

--- src/server.js
+++ src/server.js
@@ -4,20 +4,36 @@ const server = express();
 //deixando styles e script, a pasta public
 server.use(express.static('public'))
 
+
+//utilizando o template engine
+const nunjucks = require('nunjucks');
+nunjucks.configure('src/views', {
+  express: server,
+  noCache:true
+})
+
+
 //configurar caminho da aplicação, dar uma rota pra porta 3000
 //pagina home
 // req : requisição
 // res : resposta
+
+//versao para só enviar para o site, o dirname se faz necessário porque não esta renderizado pelo nunjucks
+// server.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/views/index.html')
+// })
+
+// renderizar o html puro
 server.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.render('index.html')
 })
 
 server.get('/create-point', (req, res) => {
-  res.sendFile(__dirname + '/views/create-point.html')
+  return res.render('create-point.html')
 })
 
 server.get('/search-result', (req, res) => {
-  res.sendFile(__dirname + '/views/search-result.html')
+  return res.render('search-result.html')
 })
 
 //ligar o servidor
