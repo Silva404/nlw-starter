637489844f325dc0caa7846481f8f9d8d696cc00
Silva404 <brenosilva0252@gmail.com>
2020-06-05 23:17:47 -0300

    sqlite instalado

---
+++ src/database/db.js
@@ -0,0 +1,26 @@
+// importara dependência do sqlite3
+const sqlite3 = require('sqlite3').verbose()
+
+//criar o objeto para manuzear o banco de dados
+const db = new sqlite3.Database('./src/database/database.db')
+
+//utilizando o banco de dados
+db.serialize(() => {
+  //comandos SQL
+  //1 criar uma tabela 
+  db.run(`
+  CREATE TABLE IF NOT EXISTS places (
+    
+  );
+  `)
+
+
+  //2 inserir dados na tabela
+
+
+  //3 consultar os dados
+
+
+  //4 deletar um dado
+  //tentar adicionar isso ao site, para que quem colocou o anuncio, possa também removelo
+})
